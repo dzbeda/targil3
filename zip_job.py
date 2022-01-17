a4e80ae85c64574daa99bce32f7f8a3a37f16a40
dzbeda <44648295+dzbeda@users.noreply.github.com>
2022-01-17 14:15:12 +0200

    Update zip_job.py

--- zip_job.py
+++ zip_job.py
@@ -1,28 +1,30 @@
 import logging
 import os
 import zipfile
+from pathlib import Path
 import time
 
                                                     ## Functions ##
 
 def create_file(full_file_name):
-    open(full_file_name,'w+')
+    with open(full_file_name,'w+') as f:
     #time.sleep(20)  #Using sleep will give you the option to delete the file manually in order to check the if statment in case the file was not created
-    if os.path.exists(full_file_name) == True:
-        print(full_file_name+"File exists")
-        logging.info("Created filename: " + filename + file_extension + " under " + file_path)
-    else:
-        print("Failed to create file  - exiting")
-        logging.error("Could not create filename: " + filename + file_extension + " under " + file_path + " exiting")
-        exit()
+        if os.path.exists(full_file_name) == True:
+            print(full_file_name+" File exists")
+            logging.info("Created filename: " + filename + file_extension + " under " + file_path)
+        else:
+            print("Failed to create file  - exiting")
+            logging.error("Could not create filename: " + filename + file_extension + " under " + file_path + " exiting")
+            exit()
 
 def create_zip(zip_file_name,full_file_name):
+
     handle = zipfile.ZipFile(zip_file_name,"w")
     handle.write(full_file_name,compress_type= zipfile.ZIP_DEFLATED)
     handle.close()
     #time.sleep(20)  #Using sleep will give you the option to delete the file manually in order to check the if statment in case the file was not created
     if os.path.exists(zip_file_name) == True:
-        print(zip_file_name+"ZIP File exists")
+        print(zip_file_name+" File exists")
         logging.info("Created filename: " + zip_file_name + " under " + zip_file_path)
     else:
         print("Failed to create file  - exiting")
@@ -31,19 +33,21 @@ def create_zip(zip_file_name,full_file_name):
 
                                              ####  variables   #####
 ## Log file parameters ##
-logFile = '/data/dudu-private/assignments/targil3/files/output.log'
-logging.basicConfig(filename=logFile,filemode='a',format='%(asctime)s - %(levelname)s - %(message)s' , level=logging.INFO)
+logFile = '/tmp/output.log'
+logging.basicConfig(filename=logFile,filemode='w',format='%(asctime)s - %(levelname)s - %(message)s' , level=logging.INFO)
 
 
 ## Program variables ##
 files = {'a','b','c','d'}
 file_extension= '.txt'
-file_path='/data/dudu-private/assignments/targil3/files/'  #define the path were txt files will be created
-zip_file_path= '/data/dudu-private/assignments/targil3/'   #define the path were zip files will be created
+file_path='/tmp/'  #define the path were txt files will be created
+zip_file_path= '/tmp/zip/'   #define the path were zip files will be created
 version=os.getenv('VERSION')
 
 
                                                         ## Start program ##
+
+## Check if env Version exists
 if os.getenv('VERSION') is not None:
     print(version)
     version = os.getenv('VERSION')
@@ -52,6 +56,13 @@ else:
     print("env VERSION is not defined - exiting")
     logging.error("env VERSION is not defined - exiting")
     exit()
+
+## Create folder if are not exists
+
+Path(file_path).mkdir(parents=True, exist_ok=True)
+Path(zip_file_path).mkdir(parents=True, exist_ok=True)
+
+## Generate files
 
 for filename in files:
     full_file_name = file_path+filename+file_extension
